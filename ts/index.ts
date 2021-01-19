7c4219c8f31f222bcdf66d578a973f8a2e9b2f6e
Pavan <pavan@linkeddots.com>
2021-01-19 18:14:50 +0530

    HTTPS and service section

--- ts/index.ts
+++ ts/index.ts
@@ -4,7 +4,6 @@ class Car {
 
     drive() {
         console.log("Drive");
-        
     }
 }
 
---
+++ wpsearch/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  term='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onInput(value:string) {
+
+  }
+
+}
